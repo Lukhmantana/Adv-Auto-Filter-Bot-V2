ec56418528ebccd08c925c6d9600fb65ebd36c75
Lukhmantana <87334768+Lukhmantana@users.noreply.github.com>
2022-02-21 23:49:57 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '**ᴊᴏɪɴ ɴᴏᴡ**', url="https://t.me/cinmeamax"
                                 )
                         ]
                     ]
